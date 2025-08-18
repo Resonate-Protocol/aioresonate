b2e7b2dc68831f5ded8c68a0bb236ecdbdd895ae
Maxim Raznatovski <nda.mr43@gmail.com>
2025-08-18 13:34:17 +0200

    Add av based resampling (broken for now)

--- aioresonate/server/group.py
+++ aioresonate/server/group.py
@@ -8,6 +8,8 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING
 from uuid import uuid4
 
+import av
+
 from aioresonate.models import server_messages
 
 # The cyclic import is not an issue during runtime, so hide it
@@ -171,25 +173,41 @@ class PlayerGroup:
         samples_sent = 0
 
         chunk_timestamp_us = start_time_us
+        resampler = av.AudioResampler(
+            format="s16",
+            layout="stereo",
+            rate=48000,
+        )
 
         async for chunk in audio_source:
+            logger.info("received chunk from MA")
             chunk_pos = 0
-            while True:
-                if chunk_pos >= len(chunk):
-                    break
-                c = chunk[
-                    chunk_pos : chunk_pos + TARGET_CHUNK_BYTES
-                ]  # TODO: maybe that's not so efficient?
+            if chunk_pos >= len(chunk):
+                break
+            c = chunk[
+                chunk_pos : chunk_pos + TARGET_CHUNK_BYTES
+            ]  # TODO: maybe that's not so efficient?
 
-                chunk_pos += len(c)
-                samples_in_chunk = len(c) // STREAM_SAMPLE_SIZE
-                samples_sent += samples_in_chunk
+            chunk_pos += len(c)
+            samples_in_chunk = len(c) // STREAM_SAMPLE_SIZE
+            samples_sent += samples_in_chunk
+
+            in_frame = av.AudioFrame(format="s16", layout="stereo", samples=samples_in_chunk)
+            in_frame.sample_rate = 48000
+            in_frame.planes[0].update(chunk)
+
+            logger.info("resampled!")
+
+            for out_frame in resampler.resample(in_frame):
+                logger.info("received frame from av")
+                resampled_chunk = bytes(out_frame.planes[0])
+                assert in_frame.samples == out_frame.samples
 
                 for player in self._players:
                     player.send_audio_chunk(
                         timestamp_us=chunk_timestamp_us,
                         sample_count=samples_in_chunk,
-                        audio_data=c,
+                        audio_data=resampled_chunk,
                     )
 
                 duration_of_samples_in_chunk = int(
@@ -200,6 +218,8 @@ class PlayerGroup:
                 time_until_next_chunk = chunk_timestamp_us - int(
                     self._server.loop.time() * 1_000_000
                 )
+                logger.info("next chunk in %ss", time_until_next_chunk)
 
                 if time_until_next_chunk > BUFFER_DURATION_US:
+                    logger.info("sleeping...")
                     await asyncio.sleep((time_until_next_chunk - BUFFER_DURATION_US) / 1_000_000)
