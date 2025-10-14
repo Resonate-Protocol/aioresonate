8404699a672c35e4a618bc167395fcb87c1edcc3
Maxim Raznatovski <nda.mr43@gmail.com>
2025-10-14 09:02:11 +0200

    Remove unnecessary remove_client() logging

--- aioresonate/server/group.py
+++ aioresonate/server/group.py
@@ -800,7 +800,6 @@ class ResonateGroup:
             client: The client to remove from this group.
         """
         if client not in self._clients:
-            logger.debug("client %s not in group, skipping removal", client.client_id)
             return
         logger.debug("removing %s from group with members: %s", client.client_id, self._clients)
         if len(self._clients) == 1:
