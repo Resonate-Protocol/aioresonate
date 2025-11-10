5593cf0ae8cbff31261f0806db6ef15646e13169
tegelane89 <tegelane89@gmail.com>
2025-11-10 10:14:24 +0200

    Suppress signal handler errors in CLI client on Windows (#66)
    This shows how to get resonate-cli working on windows by swallowing
    NotImplementedError.
    Since I don't know how to write python, please take this with grain of
    salt - there may be other proper ways to solve this. But from what I'm
    reading from stackoverflow swallowing may be right thing to do in this
    case:
    https://stackoverflow.com/questions/45987985/asyncio-loops-add-signal-handler-in-windows
    
    ---------
    
    Co-authored-by: Maxim Raznatovski <nda.mr43@gmail.com>

--- aioresonate/cli.py
+++ aioresonate/cli.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import argparse
 import asyncio
+import contextlib
 import logging
 import signal
 import socket
@@ -615,7 +616,9 @@ async def main_async(argv: Sequence[str] | None = None) -> int:  # noqa: PLR0915
             logger.debug("Received interrupt signal, shutting down...")
             keyboard_task.cancel()
 
-        loop.add_signal_handler(signal.SIGINT, signal_handler)
+        # Signal handlers aren't supported on this platform (e.g., Windows)
+        with contextlib.suppress(NotImplementedError):
+            loop.add_signal_handler(signal.SIGINT, signal_handler)
 
         try:
             # Run connection loop with auto-reconnect
@@ -624,7 +627,9 @@ async def main_async(argv: Sequence[str] | None = None) -> int:  # noqa: PLR0915
             logger.debug("Connection loop cancelled")
         finally:
             # Remove signal handler
-            loop.remove_signal_handler(signal.SIGINT)
+            # Signal handlers aren't supported on this platform (e.g., Windows)
+            with contextlib.suppress(NotImplementedError):
+                loop.remove_signal_handler(signal.SIGINT)
             await audio_handler.cleanup()
             await client.disconnect()
 
